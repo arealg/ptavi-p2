3ffb4d096fc035c42cd42f67276d11037619906d
Ana Real <anavm4_@hotmail.com>
2015-09-28 19:42:26 +0200

    Cambio Variables

--- calcoo.py
+++ calcoo.py
@@ -14,17 +14,17 @@ class Calculadora:
         return x-y
 
 
-def mod_operacion(calc_operac, numero1, numero2):
+def modo_operacion(calc_operac, numero1, numero2):
     solucion = calc_operac(numero1, numero2)
     print (solucion)
 
 
-def operacion_valida(operacion, numero1, numero2):
+def operando_valida(operacion, numero1, numero2):
     calculo = Calculadora()
-    op_val = {'suma': calculo.sumar, 'resta': calculo.restar}
+    libreria = {'suma': calculo.sumar, 'resta': calculo.restar}
 
     try:
-        mod_operacion(op_val[operacion], numero1, numero2)
+        modo_operacion(libreria[operacion], numero1, numero2)
     except:
         print ('Error: Non operation parameters')
 
@@ -36,8 +36,8 @@ if __name__ == '__main__':
     operacion = sys.argv[2]
 
     try:
-        numero1 = calc.find_float(sum1)
-        numero2 = calc.find_float(sum2)
-        operacion_valida(calculo, operacion, numero1, numero2, op_val)
+        numero1 = calc.es_float(sum1)
+        numero2 = calc.es_float(sum2)
+        operando_valida(operacion, numero1, numero2)
     except ValueError:
         print ('Error: Non numerical parameters')
