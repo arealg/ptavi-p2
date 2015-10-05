a38a5687ca2e680e2cda4ce11d2ffce685071661
Ana Real <anavm4_@hotmail.com>
2015-10-05 15:30:22 +0200

    Capturar Errores

--- calcoo.py
+++ calcoo.py
@@ -6,6 +6,9 @@ import calc
 
 
 class Calculadora:
+    def __init__(self):
+        self.libreria = {'suma': self.sumar, 'resta': self.restar}
+
 
     def sumar(self, x, y):
         return x+y
@@ -14,19 +17,18 @@ class Calculadora:
         return x-y
 
 
-def modo_operacion(modelo, numero1, numero2):
-    solucion = modelo(numero1, numero2)
+def modo_operacion(tipo_operacion, numero1, numero2):
+    solucion = tipo_operacion(numero1, numero2)
     print (solucion)
 
 
 def operando_valido(operacion, numero1, numero2):
     calculo = Calculadora()
-    libreria = {'suma': calculo.sumar, 'resta': calculo.restar}
 
     try:
-        modo_operacion(libreria[operacion], numero1, numero2)
-    except:
-        print ('Error: Non operation parameters')
+        modo_operacion(calculo.libreria[operacion], numero1, numero2)
+    except KeyError:
+        sys.exit('Error: Non operation parameter')
 
 
 if __name__ == '__main__':
@@ -35,9 +37,6 @@ if __name__ == '__main__':
     sum2 = sys.argv[3]
     operacion = sys.argv[2]
 
-    try:
-        numero1 = calc.es_float(sum1)
-        numero2 = calc.es_float(sum2)
-        operando_valido(operacion, numero1, numero2)
-    except ValueError:
-        print ('Error: Non numerical parameters')
+    numero1 = calc.es_float(sum1)
+    numero2 = calc.es_float(sum2)
+    operando_valido(operacion, numero1, numero2)
