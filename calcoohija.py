2d3a53782900d30934233028899697ca8cec99f2
Ana Real <anavm4_@hotmail.com>
2015-10-05 15:30:29 +0200

    Capturar Errores

--- calcoohija.py
+++ calcoohija.py
@@ -8,30 +8,28 @@ import calc
 
 class CalculadoraHija(calcoo.Calculadora):
 
-    def dividir(self, x, y):
-        try:
-            return x/y
-        except:
-            sys.exit('Division by zero is not allowed')
+    def __init__(self):
+        super().__init__()
+        self.libreria['divide'] = self.dividir
+        self.libreria['multiplica'] =  self.multiplicar
+
 
     def multiplicar(self, x, y):
         return x*y
 
-
-def modo_operacion(modelo, numero1, numero2):
-    solucion = modelo(numero1, numero2)
-    print (solucion)
-
+    def dividir(self, x, y):
+        try:
+            return x/y
+        except ZeroDivisionError:
+            sys.exit('Division by zero is not allowed')
 
 def operando_valido(operando, numero1, numero2):
-
     calculo = CalculadoraHija()
-    libreria = {'suma': calculo.sumar, 'resta': calculo.restar,
-                'multiplica': calculo.multiplicar, 'divide': calculo.dividir}
+
     try:
-        modo_operacion(libreria[operacion], numero1, numero2)
-    except:
-        print ('Error: Non operation parameters')
+        calcoo.modo_operacion(calculo.libreria[operacion], numero1, numero2)
+    except KeyError:
+        sys.exit('Error: Non operation parameter')
 
 
 if __name__ == '__main__':
@@ -40,9 +38,6 @@ if __name__ == '__main__':
     sum2 = sys.argv[3]
     operacion = sys.argv[2]
 
-    try:
-        numero1 = calc.es_float(sum1)
-        numero2 = calc.es_float(sum2)
-        operando_valido(operacion, numero1, numero2)
-    except ValueError:
-        print ('Error: Non numerical parameters')
+    numero1 = calc.es_float(sum1)
+    numero2 = calc.es_float(sum2)
+    operando_valido(operacion, numero1, numero2)
